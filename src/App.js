9896bb34ae1d21381c70f4aa8b1b5634a8a68643
Udaya-Pujari <udaysp194@gmail.com>
2024-02-25 22:47:18 +0530

    Episode 7 ,working on routers

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
-
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -16,6 +15,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+// root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>About us</h1>
+        <h2>This is Namaste React Web Series</h2>
+      </div>
+    </>
+  );
+};
+
+export default About;
