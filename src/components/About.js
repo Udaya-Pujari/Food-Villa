e32cdafb12cb566137c4f5c6fde5c5853e551645
Udaya-Pujari <udaysp194@gmail.com>
2024-04-04 21:48:51 +0530

    making api call in class based components and completion of class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,56 @@
 import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <>
-      <div>
-        <h1>About us</h1>
-        <h2>This is Namaste React Web Series</h2>
-        {/* giving/passing the props for User function */}
-        <User name={'Udaya Puajri'} />
 
-        {/* giving/passing the props to the USerClass */}
-        <UserClass name={"Udaya Pujari from class component recived props"} location={"karnataka"}/>
-      </div>
-    </>
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    // console.log("parent componentDidMount");
+  }
+
+  render() {
+    // console.log("Parent render");
+
+    return (
+      <>
+        <div>
+          <h1>About Class Component</h1>
+          <h2>This is Namaste React Web Series</h2>
+          {/* giving/passing the props for User function */}
+          <User name={"Udaya Puajri"} />
+
+          {/* giving/passing the props to the USerClass */}
+          <UserClass
+            name={"Udaya Pujari from class component recived props"}
+            location={"karnataka"}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <>
+//       <div>
+//         <h1>About us</h1>
+//         <h2>This is Namaste React Web Series</h2>
+//         {/* giving/passing the props for User function */}
+//         <User name={"Udaya Puajri"} />
+
+//         {/* giving/passing the props to the USerClass */}
+//         <UserClass
+//           name={"Udaya Pujari from class component recived props"}
+//           location={"karnataka"}
+//         />
+//       </div>
+//     </>
+//   );
+// };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,43 +1,50 @@
 import React from "react";
+import { json } from "react-router-dom";
 class UserClass extends React.Component {
   //receiving the pops from About.js
   constructor(props) {
     super(props);
-
-    //
+    // console.log(props);
     this.state = {
-      //this is the way of creating the state variable in the class component
-      count: 0,
-      count2: 1,
+      userInfo: {
+        login: "dummy name",
+        location: "default location",
+        avatar_url: "http://dummy.com",
+      },
     };
-
-    console.log(props);
   }
+
+  //In class based components you can make the componentDidmount as async as below shown
+  async componentDidMount() {
+    //API call
+    const data = await fetch("https://api.github.com/users/Udaya-Pujari-194");
+    const convertToJson = await data.json();
+    console.log(convertToJson);
+
+
+    //updating my state
+    this.setState({
+      userInfo: convertToJson,
+    });
+  }
+
+  componentDidUpdate(){
+    console.log("component did update");
+  }
+
+  componentWillUnmount(){
+    console.log("componrent will unmount");
+  }
+
   render() {
-    // insted of writing always this.props.etc every time , I can destructure it like as below
-    const { name, location } = this.props;
-    //destructuring the states on the fly
-    const { count, count2 } = this.state;
+    // const { name, location } = this.props;
+    const { login, location ,avatar_url} = this.state.userInfo;
+
     return (
       <>
         <div className="user-card">
-          {/* <h2>Name : {this.props.name}</h2>
-          <h3>Location : {this.props.location}</h3> */}
-          <h1>count : {this.state.count}</h1>
-          <button
-            onClick={() => {
-              //NOTE: Never update the state variables directly(WRONG==> this.state.count=this.state.cont+1), insted make use of this.setState
-              //CORRECT==> over here in the this.setState we pass in an object, and this object will contain the updated value of state variables
-              this.setState({
-                count: this.state.count + 1,
-              });
-            }}
-          >
-            Count Increase
-          </button>
-          {/**  i have directly used the cont2 bcz I ahve destructured: this.state  */}
-          <h2>count1 :{count2}</h2>
-          <h2>Name : {name}</h2>
+          <img src={avatar_url} />
+          <h2>Name : {login}</h2>
           <h3>Location : {location}</h3>
           <h4>Contact : udaysp194@gmail.com</h4>
         </div>
